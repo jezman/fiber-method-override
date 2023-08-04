60cf24ba3a6ba7d9d984414f7ff0a30a8e66cda7
jezman <s3420052@yandex.ru>
2023-08-04 11:41:42 +0300

    init

---
+++ method_override.go
@@ -0,0 +1,36 @@
+package methodOverride
+
+import (
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func New() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		// processing only post requests
+		if c.Method() != fiber.MethodPost {
+			return c.Next()
+		}
+
+		// checking the hidden field "_method"
+		_method := strings.ToUpper(c.FormValue("_method"))
+
+		if _method == "" {
+			return c.Next()
+		}
+
+		switch _method {
+		case fiber.MethodPut:
+			c.Method(fiber.MethodPut)
+		case fiber.MethodPatch:
+			c.Method(fiber.MethodPatch)
+		case fiber.MethodDelete:
+			c.Method(fiber.MethodDelete)
+		default:
+			return c.Next()
+		}
+
+		return c.Next()
+	}
+}
